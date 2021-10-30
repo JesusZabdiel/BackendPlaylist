827b02198acbf31aba23a4ef26388fbff0bdc846
JesusZabdiel <A01374964@itesm.mx>
2021-10-30 11:23:44 -0500

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const { request, response, query } = require('express');
+const express = require('express');
+
+
+const app = express();
+const sequelize = require('./utils/database')
+const plyRoutes = require('./routes/playlists')
+
+
+//JSON
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.use('/playlists' , plyRoutes)
+
+mongoose.connect('mongodb://user7:root@54.173.202.133:27017/base7?authSource=admin')
+.then(()=>{
+    app.listen(8080,()=>console.log("Servidor en linea"))
+})
+.catch(err=>console.log(err))
